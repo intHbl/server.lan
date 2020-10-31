178bda4b98b0344a637f8dbee73bc99c5b509f6f
Your Name <you@example.com>
2020-10-31 14:52:32 +0800

    v1.00000000

--- docker/docker.seafile-v7.0.5/docker.seafile-v7.0.5-baselayer/armhf.pre_build.sh
+++ docker/docker.seafile-v7.0.5/docker.seafile-v7.0.5-baselayer/armhf.pre_build.sh
@@ -5,10 +5,18 @@
     cd "`dirname "$0"`"
     
     cp Dockerfile armhf.Dockerfile.__gen__
+    
+    _download_file="seafile-server_7.0.5_stable_pi.tar.gz"
+    _download_url="https://github.com/haiwen/seafile-rpi/releases/download/v7.0.5/seafile-server_7.0.5_stable_pi.tar.gz"
 
-    if [  ! -e "seafile-server_7.0.5_stable_pi.tar.gz" ];then
-        wget https://github.com/haiwen/seafile-rpi/releases/download/v7.0.5/seafile-server_7.0.5_stable_pi.tar.gz \
-            -O seafile-server_7.0.5_stable_pi.tar.gz
+    if [ ! -s "${_download_file}" ];then
+        test -e "${_download_file}"  && rm "${_download_file}" 
+        wget  "${_download_url}"  -O "${_download_file}"
+    fi
+
+    if [ ! -s "${_download_file}" ];then
+        test -e "${_download_file}"  && rm "${_download_file}"
+        exit 1
     fi
 
 )
