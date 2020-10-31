9b1960282020e6ff540f1acae0097c4b6b4b411d
Your Name <you@example.com>
2020-10-31 13:37:12 +0800

    v1.0000000

--- docker/docker.seafile-v7.0.5/docker.seafile-v7.0.5-baselayer/armhf.pre_build.sh
+++ docker/docker.seafile-v7.0.5/docker.seafile-v7.0.5-baselayer/armhf.pre_build.sh
@@ -3,7 +3,13 @@
 (
 
     cd "`dirname "$0"`"
+    
     cp Dockerfile armhf.Dockerfile.__gen__
+
+    if [  ! -e "seafile-server_7.0.5_stable_pi.tar.gz" ];then
+        wget https://github.com/haiwen/seafile-rpi/releases/download/v7.0.5/seafile-server_7.0.5_stable_pi.tar.gz \
+            -O seafile-server_7.0.5_stable_pi.tar.gz
+    fi
 
 )
 
