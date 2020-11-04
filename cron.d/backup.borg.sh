6c0d7052435e2ba5950c18eeb18287fbf856227c
Your Name <you@example.com>
2020-11-04 17:40:45 +0800

    v1.0000x31

--- cron.d/backup.borg.sh
+++ cron.d/backup.borg.sh
@@ -20,50 +20,44 @@ logs__="${base_dir_data}/data.static_file/logs__"
 
 (
 	echo "[INFO]::borg backup :: `date` "
+	echo 
 	echo "[INFO] backup for     ${__SERVICE_NAME__} "
-	# .../${__SERVICE_NAME__}_server
-	backup_base_path="${base_dir_backup}"
-	data_base_path="${base_dir_data}"
-	# .../data.${__SERVICE_NAME__}
+	
+	## from
+	data_path_base="${base_dir_data_mnt}"
+	## to
+	backup_path_base="${base_dir_backup_mnt}"
+	
+	# docker container :: .../${__SERVICE_NAME__}_server.lan
+	# dir :: .../data.${__SERVICE_NAME__}  ->  .../backup.${__SERVICE_NAME__}
 
-	{
-		if [ -z "${backup_base_path}" ];then
-			echo "[Err] :: please set arg :: 'backup_base_path' "
+	function check_ {
+		if [ -z "${backup_path_base}" ];then
+			echo "[Err] :: please set arg :: 'backup_path_base' <- 'base_dir_backup_mnt' "
+			exit 1
+		fi
+		if [ -z "${data_path_base}" ];then
+			echo "[Err] :: please set arg :: 'data_path_base' <- 'base_dir_data_mnt' "
 			exit 1
 		fi
 
-		if [ ! -d "${backup_base_path}" ];then
-			echo "[Err] :: is not a dir :: ${backup_base_path} "
-			exit 1
-		fi
-
-		if  ! mountpoint ${backup_base_path} ;then
+		if  ! mountpoint ${backup_path_base} ;then
 			echo "[Err] `date`     ::disk::backup is not mounted" 
 			# send message to --> TODO
 			exit 1
 		fi
 
-
-		if [ -z "${data_base_path}" ];then
-			echo "[Err] :: please set arg :: 'data_base_path' "
-			exit 1
-		fi
-
-		if [ ! -d "${data_base_path}" ];then
-			echo "[Err] :: is not a dir :: ${data_base_path} "
-			exit 1
-		fi
-
-		if  ! mountpoint ${data_base_path} ;then
+		if  ! mountpoint ${data_path_base} ;then
 			echo "[Err] disk::data is not mounted"
 			exit 1
 		fi
 	}
 
+	check_ 
 	
 
 	###############
-	_repo_path=${backup_base_path}/backup.${__SERVICE_NAME__}
+	_repo_path=${backup_path_base}/backup.${__SERVICE_NAME__}
 	export BORG_PASSPHRASE=mima123456
 
 	if [ ! -e ${_repo_path} ];then
@@ -77,7 +71,7 @@ logs__="${base_dir_data}/data.static_file/logs__"
 		docker stop "${_containername}"
 
 		tag=`date +%Y%m%d_%H%M%S`
-		if ! borg  create  --stats -p  ${_repo_path}::${tag}  ${data_base_path}/data.${__SERVICE_NAME__};then
+		if ! borg  create  --stats -p  ${_repo_path}::${tag}  ${data_path_base}/data.${__SERVICE_NAME__};then
 			echo "[Err]::`date`   :: borg backup err."
 			# send message to --> TODO
 		fi
--- cron.d/backup.remote.sh
+++ cron.d/backup.remote.sh
@@ -21,65 +21,57 @@ source  "${_configfile}"
 logs__="${base_dir_data}/data.static_file/logs__"
 
 (
-
+	echo "[INFO]::remote backup :: `date` "
+	echo 
 	echo "[INFO] backup for     ${__SERVICE_NAME__} "
-	# .../${__SERVICE_NAME__}_server
+
     # from
-	borg_base_path="${base_dir_backup}"
-	borg_repo_path=${borg_base_path}/backup.${__SERVICE_NAME__}
+	borg_repo_path="${base_dir_data_mnt}/backup.${__SERVICE_NAME__}"
     # to
-	remote_base_path="TODO"
+	remote_="${username_}@${remote_host_}"
+	remote_path="${remote}:${borg_repo_path}"
+	remote_ssh_key_="/home/${username_}/.ssh/${remote_host_}_id_rsa"
+	function remote_backup_func_ {
+		rsync -r  -i "${remote_ssh_key_}" "${borg_repo_path}"   "${remote_path}"
+	}
 	# .../data.${__SERVICE_NAME__}
 
-    # check directories! TODO
+    # check
+	## directories
 	{
-		echo "[INFO]::remote backup :: `date` "
-		echo "[INFO] backup for     ${__SERVICE_NAME__} "
-
-		if [ -z "${backup_base_path}" ];then
-			echo "[Err] :: please set arg :: 'backup_base_path' "
+		if [ -z "${base_dir_data_mnt}" ];then
+			echo "[Err] :: please set arg :: 'backup_base_path' -> 'base_dir_backup_mnt' "
 			exit 1
 		fi
 
-		if [ ! -d "${backup_base_path}" ];then
-			echo "[Err] :: is not a dir :: ${backup_base_path} "
-			exit 1
-		fi
-
-		if  ! mountpoint ${backup_base_path} ;then
+		if  ! mountpoint ${base_dir_data_mnt} ;then
 			echo "[Err] disk::backup is not mounted"
 			exit 1
 		fi
-
-
-		if [ -z "${data_base_path}" ];then
-			echo "[Err] :: please set arg :: 'data_base_path' "
-			exit 1
+	}
+	## ssh
+	{
+		if [ -z "${remote_host_}" ];then
+			# default ->  backup.lan
+			# set ssh rsa key => ssh/id_rsa
+			echo "[Warning] :: rsync exit:: `date` :: remote host did not set"
+			exit 0
 		fi
-
-		if [ ! -d "${data_base_path}" ];then
-			echo "[Err] :: is not a dir :: ${data_base_path} "
-			exit 1
-		fi
-
-		if  ! mountpoint ${data_base_path} ;then
-			echo "[Err] disk::data is not mounted"
+		
+		if [ ! -e "${remote_ssh_key_}" ];then
+			echo "[Err] ssh private key not exists :: ${remote_ssh_key_}"
 			exit 1
 		fi
 	}
-
 	
 
-	## rsync -r 
-	# TODO :: rsync
-	if [ -z "${remote_host_}" ];then
-		echo "[Warning] :: rsync exit:: `date` :: remote host did not set"
-		exit 0
-	fi
-	remote_dir="${username_}@${remote_host_}:/${base_dir_backup}/backup.${__SERVICE_NAME__}"
-	## rync -r  "${_repo_path}"   "${remote_dir}"
+	echo "[Info] starting rsync ...:: ${borg_repo_path} -> ${remote_path}"
+	echo " - - - - - - - - - - - - - - - - - - - - - - "
+	remote_backup_func_
 
-	#TODO
-	echo TODO
+
+	echo ""
+	echo "[Info] DONE"
+	echo " _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ "
 
 )  2>&1 | tee "${base_dir_log}/backup.${__SERVICE_NAME__}.remote.$((`date "+%d"`%10)).log" | tee  "${logs__}/backup.${__SERVICE_NAME__}.remote.log"
